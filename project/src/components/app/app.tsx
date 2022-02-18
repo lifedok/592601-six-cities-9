2bb0fce82335805ebd570e3441d8aa540f50e5e7
aleksey <lifedok@gmail.com>
2022-02-18 17:48:25 +0200

    Add enums for route and add routes for SPA

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../layout/layout';
+import { NotFound } from '../../pages/not-found/not-found';
+import { AuthorizationStatus, ERoute } from "../../types/enums/route.enum";
+import LoginPage from "../../pages/login-page/login-page";
+import FavoritesPage from "../../pages/favorites-page/favorites-page";
+import PrivateRoute from "../private-route/private.route";
 
 
 export default function App(): JSX.Element {
-  return <Layout/>;
+
+  const isLogged = AuthorizationStatus.AUTH;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Layout/>}/>
+        <Route path={ERoute.LOGIN} element={<LoginPage/>}/>
+        <Route
+          path={ERoute.FAVORITES}
+          element={
+            <PrivateRoute authorizationStatus={isLogged}>
+              <FavoritesPage/>
+            </PrivateRoute>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ project/src/types/enums/route.enum.ts
@@ -0,0 +1,12 @@
+export const ERoute = {
+  MAIN: '/',
+  LOGIN: '/login',
+  FAVORITES: '/favorites',
+  ROOM: '/offer',
+};
+
+export enum AuthorizationStatus {
+  AUTH = 'AUTH',
+  NO_AUTH = 'NO_AUTH',
+  UNKNOWN = 'UNKNOWN',
+}
