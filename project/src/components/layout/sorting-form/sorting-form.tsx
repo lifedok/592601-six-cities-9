c8d93515540462e2cf18338d702ca19c4499fa11
aleksey <lifedok@gmail.com>
2022-03-26 15:02:11 +0200

    connected sortingForm

--- project/src/components/layout/sorting-form/sorting-form.tsx
+++ project/src/components/layout/sorting-form/sorting-form.tsx
@@ -1,19 +1,54 @@
+import React, { useState } from "react";
+
+export const ESorting = {
+  POPULAR: 'Popular',
+  LOW_TO_HIGH: 'Price: low to high',
+  HIGH_TO_LOW: 'Price: high to low',
+  TOP_RATED_FIRST: 'Top rated first',
+};
+
 export default function SortingForm(): JSX.Element {
+  const [isOpen, setOpen] = useState(false);
+  const [isSelectOption, setSelectOption] = useState(Object.keys(ESorting)[0]);
+
+
+  const selectedOptionItem = (event: React.MouseEvent<HTMLElement>) => {
+
+    event.preventDefault();
+    const selectedOption = event.currentTarget.innerText;
+    setOpen(false);
+    setSelectOption(selectedOption);
+  };
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setOpen(!isOpen)}>
         Popular
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"/>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-        <li className="places__option" tabIndex={0}>Price: low to high</li>
-        <li className="places__option" tabIndex={0}>Price: high to low</li>
-        <li className="places__option" tabIndex={0}>Top rated first</li>
+
+
+      <ul className={`places__options places__options--custom ${isOpen ? 'places__options--opened' : ''}`}>
+        {
+          Object.keys(ESorting).map((option, index) => {
+            const value = Object.values(ESorting)[index];
+            return (
+              <li
+                onClick={(ev) => selectedOptionItem(ev)}
+                className={`places__option ${value === isSelectOption ? 'places__option--active' : ''}`}
+                tabIndex={0}
+                key={option}
+              >
+                {value}
+              </li>
+            )
+          })
+        }
       </ul>
+
     </form>
   );
 }
