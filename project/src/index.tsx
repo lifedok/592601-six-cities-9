2b7475f9f8660ce0f738868dfadac4d2f6bd974e
aleksey <lifedok@gmail.com>
2022-04-01 03:18:47 +0300

    Fetch data for offers & check auth

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { checkAuthAction, fetchOffersAction } from './store/api-actions';
+
+store.dispatch(fetchOffersAction());
+store.dispatch(checkAuthAction());
 
 ReactDOM.render(
   <React.StrictMode>
