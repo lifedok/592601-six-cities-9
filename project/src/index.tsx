b103db3a158709503316c5e849c7f21214f9e1e7
aleksey <lifedok@gmail.com>
2022-04-01 03:18:13 +0300

    Add new lib - react-toastify and connect it

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -7,6 +7,7 @@ import { store } from './store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App/>
     </Provider>
   </React.StrictMode>,
