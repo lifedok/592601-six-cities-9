bb9d84238173a96852dbb1eaa1ea55f6cc1afa61
aleksey <lifedok@gmail.com>
2022-04-01 03:17:34 +0300

    Expand route enums and types for auth-data, error and user-data

--- project/src/types/enums/route.enum.ts
+++ project/src/types/enums/route.enum.ts
@@ -6,8 +6,22 @@ export const ERoute = {
   LOCATION: '/location',
 };
 
+export enum ApiRoute {
+  LOGIN = '/login', // проверить статус авторизации пользователя, aвторизоваться на сервере
+  LOGOUT = '/logout', // завершить сеанс пользователя
+  HOTELS = '/hotels', // получить список предложений
+  FAVORITES = '/favorite',  // получить список избранных предложений
+  COMMENTS = '/comments', // получить список комментариев
+}
+
 export enum AuthorizationStatus {
   AUTH = 'AUTH',
   NO_AUTH = 'NO_AUTH',
   UNKNOWN = 'UNKNOWN',
+}
+
+export enum HTTP_CODE {
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
 }
