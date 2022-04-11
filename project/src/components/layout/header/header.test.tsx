4a1aef74bf850531c8462fbd0a9cbdb0bc210af4
aleksey <lifedok@gmail.com>
2022-04-11 06:55:22 +0300

    Add test for Header

--- project/src/components/layout/header/header.test.tsx
+++ project/src/components/layout/header/header.test.tsx
@@ -4,6 +4,7 @@ import HistoryRouter from '../../history-route/history-route';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
 import Header from './header';
+import { AuthorizationStatus } from "../../../types/enums/route.enum";
 
 
 const mockStore = configureMockStore();
@@ -16,16 +17,18 @@ describe('Component: Header', () => {
   });
 
   it('should render correctly', () => {
-    // const store = mockStore();
-    //
-    // render(
-    //   <Provider store={store}>
-    //     <HistoryRouter history={history}>
-    //       <Header/>
-    //     </HistoryRouter>
-    //   </Provider>
-    // );
-    //
-    // expect(screen.getByTestId('header')).toBeInTheDocument();
+    const store = mockStore({
+      USER: {authorizationStatus: AuthorizationStatus.AUTH},
+    });
+
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <Header/>
+        </HistoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByTestId('header')).toBeInTheDocument();
   });
 });
