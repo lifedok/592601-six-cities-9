69371782d0c6db63bd49769fa9812d5abad5aa11
aleksey <lifedok@gmail.com>
2022-04-11 07:22:03 +0300

    remove userEvent, because in the git has a error (Cannot find module '@testing-library/dom' from 'node_modules/@testing-library/user-event/dist/click.js')

--- project/src/pages/login-page/login-page.test.tsx
+++ project/src/pages/login-page/login-page.test.tsx
@@ -2,7 +2,6 @@ import {render, screen} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import {configureMockStore} from '@jedmao/redux-mock-store';
 import {Provider} from 'react-redux';
-import userEvent from '@testing-library/user-event';
 import LoginPage from './login-page';
 import HistoryRouter from '../../components/history-route/history-route';
 import { AuthorizationStatus } from '../../types/enums/route.enum';
@@ -23,17 +22,10 @@ describe('Component: LoginPage', () => {
         <HistoryRouter history={history}>
           <LoginPage />
         </HistoryRouter>
-      </Provider>,
-    );
+      </Provider>);
 
     expect(screen.getByText(/Amsterdam/i)).toBeInTheDocument();
     expect(screen.getByText(/E-mail/i)).toBeInTheDocument();
     expect(screen.getByText(/Password/i)).toBeInTheDocument();
-
-    userEvent.type(screen.getByTestId('E-mail'), 'keks@gamil.com');
-    userEvent.type(screen.getByTestId('Password'), '123456');
-
-    expect(screen.getByDisplayValue(/keks/i)).toBeInTheDocument();
-    expect(screen.getByDisplayValue(/123456/i)).toBeInTheDocument();
   });
 });
